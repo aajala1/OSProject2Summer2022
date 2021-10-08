2ece91f4e93b439e9fa02442f5eea27d13f4b54e
ateya <ateyaterence@gmail.com>
2021-10-08 16:55:12 -0500

    Resolved merge conflicts in p1.cpp

--- p1.cpp
+++ p1.cpp
@@ -15,16 +15,8 @@ using namespace std;
 // extern void *account_thread(void *);
 
 /* global application variables */
-<<<<<<< HEAD
-
-int buffer[BUF_SIZE];
-sem_t buffer_access;
-SavingsAccount savings_account_stats;
-CheckingAccount checking_account_stats;
-=======
 // int buffer[5];
 // sem_t buffer_access;
->>>>>>> 4dd6e60574990377fe18cdd1dcbd78a986d7731d
 
 int main(int argc, char *argv[])
 {
